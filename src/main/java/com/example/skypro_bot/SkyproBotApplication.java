64c2a862c0792269ce7e14429d04da47f461ea78
Olga Kargan <olgakargan72@gmail.com>
2022-10-31 20:03:12 +0400

    первый коммит

---
+++ src/main/java/com/example/skypro_bot/SkyproBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.skypro_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skypro_bot/SkyproBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skypro_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
